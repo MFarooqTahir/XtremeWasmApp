e942080d27c45999beea9e91713c87e45415a358
Farooq <Farooqtahir1234@gmail.com>
2022-05-01 02:16:01 +0500

    madeSmall

--- XtremeWasmApp/Program.cs
+++ XtremeWasmApp/Program.cs
@@ -14,8 +14,8 @@ using XtremeWasmApp;
 using XtremeWasmApp.Models;
 using XtremeWasmApp.Services;
 
-var HttpDataVar = new HttpData() { BaseAddress = new("http://localhost:2144/") };
-//var HttpDataVar = new HttpData() { BaseAddress = new("https://xt.discreetnotation.com:8445/") };
+//var HttpDataVar = new HttpData() { BaseAddress = new("http://localhost:2144/") };
+var HttpDataVar = new HttpData() { BaseAddress = new("https://xt.discreetnotation.com:8445/") };
 
 var cli = new HttpClient();
 cli.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
