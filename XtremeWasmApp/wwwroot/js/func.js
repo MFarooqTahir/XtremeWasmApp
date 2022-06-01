230ebbb44f0cd0b77c2a818fae0582709c97233b
Farooq <Farooqtahir1234@gmail.com>
2022-06-01 23:50:02 +0500

    upDate

--- XtremeWasmApp/wwwroot/js/func.js
+++ XtremeWasmApp/wwwroot/js/func.js
@@ -1,6 +1,8 @@
 ﻿function noScroll(id) {
     document.getElementById(id).onfocus = function (e) {
-        e.preventDefault(); e.stopPropagation();
+        e.preventDefault();
+        e.stopPropagation();
         window.scrollTo(0, 0);
+        document.body.scrollTop = 0;
     }
 }
