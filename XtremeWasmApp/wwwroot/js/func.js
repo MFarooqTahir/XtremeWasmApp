d8bd20934feeeb4d9dd9f7bd3f2e13f47bf12f49
Farooq <Farooqtahir1234@gmail.com>
2022-06-02 00:08:12 +0500

    upn

--- XtremeWasmApp/wwwroot/js/func.js
+++ XtremeWasmApp/wwwroot/js/func.js
@@ -1,8 +1,14 @@
-﻿function noScroll(id) {
-    document.getElementById(id).onfocus = function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-        window.scrollTo(0, 0);
-        document.body.scrollTop = 0;
+﻿function noScroll() {
+    var funct = (id) => {
+        document.getElementById(id).onfocus = function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+            window.scrollTo(0, 0);
+            document.body.scrollTop = 0;
+            setTimeout(function () { window.scrollTo(0, 0); }, 0);
+        }
     }
+    funct('MixDigitInput');
+    funct('Prize1');
+    funct('Prize2');
 }
