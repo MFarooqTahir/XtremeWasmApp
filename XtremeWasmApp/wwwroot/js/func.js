6b3a41874c7c377e58e24eea7c6d917f65816946
Farooq <Farooqtahir1234@gmail.com>
2022-06-03 00:26:45 +0500

    u

--- XtremeWasmApp/wwwroot/js/func.js
+++ XtremeWasmApp/wwwroot/js/func.js
@@ -7,6 +7,13 @@
             document.body.scrollTop = 0;
             setTimeout(function () { window.scrollTo(0, 0); }, 0);
         }
+        document.getElementById(id). = function (e) {
+            e.preventDefault();
+            e.stopPropagation();onblur
+            window.scrollTo(0, 0);
+            document.body.scrollTop = 0;
+            setTimeout(function () { window.scrollTo(0, 0); }, 0);
+        }
     }
     funct('MixDigitInput');
     funct('Prize2');
