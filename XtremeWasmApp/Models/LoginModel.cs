b90996da816c5605dfc58bfae5b614941a733111
Farooq <Farooqtahir1234@gmail.com>
2022-06-21 23:45:14 +0500

    6 digit long password

--- XtremeWasmApp/Models/LoginModel.cs
+++ XtremeWasmApp/Models/LoginModel.cs
@@ -7,7 +7,7 @@ namespace XtremeWasmApp.Models
         [Required]
         public string Email { get; set; }
 
-        [Required, RegularExpression(".{6,}", ErrorMessage = "Password must be atleast 6 characters long")]
+        [Required, RegularExpression("[0-9]{6}", ErrorMessage = "Password must be 6 digit long")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
     }
